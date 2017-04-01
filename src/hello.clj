76b5f96228ae6e94dc3708be00a65b80c47f5cad
SundarJ <sundarjoshi@hotmail.co.uk>
2017-04-01 02:12:45 +0100

    Continue with Pedestal guide (part two)

--- src/hello.clj
+++ src/hello.clj
@@ -2,8 +2,30 @@
   (:require [io.pedestal.http :as http]          
             [io.pedestal.http.route :as route]))
 
+(defn ok
+  ([body]
+    {:status 200 :body body}))
+
+(defn bad-request []
+    {:status 400 :body "Bad request"})
+
+(defn not-found []
+  {:status 404 :body "Not found"})
+
+(defn greeting-for [name]
+  (if-not (empty? name)
+    (str "Hello, " name)))
+
+(def unmentionables #{"YHWH" "Voldemort" "Mxyzptlk" "Rumplestiltskin" "曹操"})
+
 (defn respond-hello [request]
-  {:status 200 :body "Hello, world!"})
+  (let [name (get-in request [:query-params :name])
+        greeting (greeting-for name)
+        resp (cond
+               (unmentionables name) (not-found)
+               greeting (ok greeting)
+               :else (bad-request))]
+    resp))
 
 (def routes
   (route/expand-routes
