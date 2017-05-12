4080460f5d0b5ea0b169a614e3888027563d018d
Sundar Joshi <sundarjoshi@hotmail.co.uk>
2017-05-12 17:03:09 +0000

    Add list-create and list-view functions to API

--- api/src/main.clj
+++ api/src/main.clj
@@ -19,20 +19,51 @@
 
 (defonce database (atom {}))
 
-(def database-interceptor
-  {:name :database-interceptor
-  :enter (fn [context]
-           (update context :request assoc :database @database))
+(def handle-data
+  {:name :handle-data
+   :enter (fn [context]
+            (update context :request assoc :database @database))
    :leave (fn [context]
-           (if-let [[op & args] (:tx-data context)]
-             (do
-               (apply swap! database op args)
-               (assoc-in context [:request :database] @database))
-             context))})
+            (if-let [[op & args] (:tx-data context)]
+              (do
+                (apply swap! database op args)
+                (assoc-in context [:request :database] @database))
+              context))})
+              
+(defn make-list [namae]
+  {:name namae
+   :items {}})
+   
+(defn make-list-item [namae]
+  {:name namae
+   :done? false})
+   
+(def list-create
+  {:name :list-create
+   :enter (fn [context]
+            (let [namae    (get-in context [:request :query-params :name] "Unnamed List")
+                  new-list (make-list namae)
+                  db-id    (str (gensym "1"))
+                  url      (route/url-for :list-view :params {:list-id db-id})]
+              (assoc context
+                     :response (created new-list "Location" url)
+                     :tx-data [assoc db-id new-list])))})
 
- (def routes
+(def find-by-id get)
+
+(def list-view
+  {:name :list-view
+   :enter (fn [context]
+            (if-let [db-id (get-in context [:request :params :list-id])]
+              (let [db (get-in context [:request :database])]
+                (if-let [the-list (find-by-id db db-id)]
+                  (assoc context :result the-list)
+                  context)
+                context))}))
+
+(def routes
   (route/expand-routes
-   #{["/todo"                :post   echo :route-name :list-create]
+   #{["/todo"                :post   [handle-data list-create]]
      ["/todo"                :get    echo :route-name :list-query-form]
      ["/todo/:list-id"       :get    echo :route-name :list-view]
      ["/todo/:list-id"       :post   echo :route-name :list-item-create]
@@ -61,3 +92,7 @@
 (defn restart []
   (stop-dev)
   (start-dev))
+  
+(defn test-request [verb url]
+  (io.pedestal.test/response-for (::http/service-fn @server) verb url))
+  
